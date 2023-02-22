a7fd396cbaf7507b0a568682e39c33a676844eaf
thatlittlemoonfromstarwars <116924824+thatlittlemoonfromstarwars@users.noreply.github.com>
2023-02-21 20:15:18 -0500

    Added board styling and tested printing

--- Backgammon.cpp
+++ Backgammon.cpp
@@ -1,86 +1,121 @@
-
+/*
  13 14 15 16 17 18      19 20 21 22 23 24
-  |  |  |  |  |  |   |   |  |  |  |  |  |
-  |  |  |  |  |  |   |   |  |  |  |  |  |
-  |  |  |  |  |  |   |   |  |  |  |  |  |
-  |  |  |  |  |  |   |   |  |  |  |  |  |
-     |     |     |   |      |     |     |
+ _________________________________________
+  |  |  |  |  |  |   |   |  |  |  |  |  | 
+  |  |  |  |  |  |   |   |  |  |  |  |  | 
+  |  |  |  |  |  |   |   |  |  |  |  |  | 
+  |  |  |  |  |  |   |   |  |  |  |  |  | 
+     |     |     |   |      |     |     | 
                      |
   |     |     |      |   |     |     |   
   |  |  |  |  |  |   |   |  |  |  |  |  |
   |  |  |  |  |  |   |   |  |  |  |  |  |
   |  |  |  |  |  |   |   |  |  |  |  |  |
   |  |  |  |  |  |   |   |  |  |  |  |  |
+ ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
  12 11 10  9  8  7       6  5  4  3  2  1
 
-
+ 13 14 15 16 17 18      19 20 21 22 23 24
+——————————————————————————————————————————
+  |  |  |  |  |  |   |   |  |  |  |  |  | 
+  |  |  |  |  |  |   |   |  |  |  |  |  | 
+  |  |  |  |  |  |   |   |  |  |  |  |  | 
+  |  |  |  |  |  |   |   |  |  |  |  |  | 
+     |     |     |   |      |     |     | 
+                     |
+  |     |     |      |   |     |     |   
+  |  |  |  |  |  |   |   |  |  |  |  |  |
+  |  |  |  |  |  |   |   |  |  |  |  |  |
+  |  |  |  |  |  |   |   |  |  |  |  |  |
+  |  |  |  |  |  |   |   |  |  |  |  |  |
+——————————————————————————————————————————
+ 12 11 10  9  8  7       6  5  4  3  2  1
+*/
 
 #include <cstdlib>
 #include <cmath>
 #include <iostream>
 
+using namespace std;
+
 const short int SPACES = 24;
 
-void printBoard(int[SPACES]);
-void setupBoard(int board[SPACES])
+void printBoard(short int board[SPACES]);
+void setupBoard(short int board[SPACES]);
 
 int main()
 {
 	short int board[SPACES] = {0}; // makes up board
 	// negative integers correspond to red pieces
 	// positive integers correspond to white pieces
+	setupBoard(board);
+	printBoard(board);
 
 	return EXIT_SUCCESS;
 }
 
-void printBoard(int board[SPACES])
+void printBoard(short int board[SPACES])
 {
-	cout << " 13 14 15 16 17 18      19 20 21 22 23 24";
+	cout << endl;
+	cout << " 13 14 15 16 17 18      19 20 21 22 23 24" << endl;
+	cout << " ————————————————————————————————————————" << endl;
+
 	for(int row = 1; row <= 5; row++)
 	{
 		for(int col = SPACES/2 + 1; col <= SPACES; col++)
 		{
 			
 			if(board[col-1] >= row)
-				cout << " 0";
+				cout << "  0";
 			
 			else if(board[col-1] <= -row)
-				cout << " O";
+				cout << "  O";
 			
-			else:
+			else if(row == 5 && col%2 == 1)
+				cout << "   ";
+			
+			else
 				cout << "  |";
 				
-			if(col == SPACES*3/4-1)
-			{
+			if(col == SPACES*3/4)
 				cout << "   | ";
-			}
+
 		}
-		cout << break;
+		cout << endl;
 	}
-	cout << "                     |";
+
+	cout << "                     |" << endl;
+	
 	for(int row = 5; row >= 1; row--)
 	{
 		for(int col = SPACES/2; col > 0; col--)
 		{
 			if(board[col-1] >= row)
-				cout << " 0";
+				cout << "  0";
 			
 			else if(board[col-1] <= -row)
-				cout << " O";
+				cout << "  O";
 			
-			else:
+			else if(row == 5 && col%2 == 1)
+				cout << "   ";
+
+			else
 				cout << "  |";
 				
-			if(col == SPACES/4-1)
+			if(col == SPACES/4+1)
 			{
 				cout << "   | ";
 			}
 		}
+		cout << endl;
 	}
-	cout <<  "12 11 10  9  8  7       6  5  4  3  2  1";
+
+	cout << " ————————————————————————————————————————" << endl;
+	cout <<  " 12 11 10  9  8  7       6  5  4  3  2  1";
+	
 } // end printBoard()
 
-void setupBoard(int board[SPACES])
+void setupBoard(short int board[SPACES])
 {
 	board[24 -1] = -2;
 	board[1 -1] = 2;
